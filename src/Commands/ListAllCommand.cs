cb902ec3c28391630166719a5f358eeecbfc1457
Mads Kristensen <madskristensen@users.noreply.github.com>
2022-08-22 08:22:06 -0700

    Added rating prompt [release]

--- src/Commands/ListAllCommand.cs
+++ src/Commands/ListAllCommand.cs
@@ -13,6 +13,8 @@ namespace ShowKeybindings
     [Command(PackageIds.ListAll)]
     internal sealed class ListAllCommand : BaseCommand<ListAllCommand>
     {
+        private static readonly RatingPrompt _rating = new("MadsKristensen.ShowKeybindings", Vsix.Name, General.Instance, 2);
+
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
             SaveFileDialog dialog = new()
@@ -34,6 +36,9 @@ namespace ShowKeybindings
                 }
 
                 Process.Start(dialog.FileName);
+
+                await Task.Delay(5000);
+                _rating.RegisterSuccessfulUsage();
             }
         }
 
---
+++ src/Options/General.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace ShowKeybindings
+{
+    //internal partial class OptionsProvider
+    //{
+    //    // Register the options with this attribute on your package class:
+    //    // [ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), "ShowKeybindings", "General", 0, 0, true, SupportsProfiles = true)]
+    //    [ComVisible(true)]
+    //    public class GeneralOptions : BaseOptionPage<General> { }
+    //}
+
+    public class General : BaseOptionModel<General>, IRatingConfig
+    {
+        [Browsable(false)]
+        public int RatingRequests { get; set; }
+    }
+}
